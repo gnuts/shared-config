dcda1c6c91825e8719da2430b6f24874d5ee1078
Matthias Nuessle <nuts@saukopf.de>
2013-12-23 16:42:04 +0100

    added a monitor selection menu

---
+++ i3/monitordmenu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+###
+
+layoutdir=~/.screenlayout 
+
+DMENU='dmenu -i -nb #990000 -nf #999999 -sb #999999 -sf #000000'
+#choice=$(echo -e "notebook\nworkstation\nreboot\nsuspend\nhibernate" | $DMENU)
+
+choice=$((cd $layoutdir && ls -1) | $DMENU)
+
+
+
