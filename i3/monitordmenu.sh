f4b62b3c17acaa4ca919e8d2ab9ed5284a4a7d48
Matthias Nuessle <nuts@saukopf.de>
2013-12-23 16:45:35 +0100

    made monitor menu look nicer :)

--- i3/monitordmenu.sh
+++ i3/monitordmenu.sh
@@ -7,7 +7,9 @@ layoutdir=~/.screenlayout
 DMENU='dmenu -i -nb #990000 -nf #999999 -sb #999999 -sf #000000'
 #choice=$(echo -e "notebook\nworkstation\nreboot\nsuspend\nhibernate" | $DMENU)
 
-choice=$((cd $layoutdir && ls -1) | $DMENU)
+choice=$((cd $layoutdir && ls -1|perl -pe 's/\.sh$//') | $DMENU)
 
-
-
+script="$layoutdir/${choice}.sh"
+if [ -x "$script" ]; then
+    "$script"
+fi
