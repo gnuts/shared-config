422d089fe901dca1fe656c728e74c57bac0b0cd3
Matthias Nuessle <nuts@saukopf.de>
2013-12-23 16:47:58 +0100

    use standard colors for monitor dmenu

--- i3/monitordmenu.sh
+++ i3/monitordmenu.sh
@@ -4,10 +4,10 @@
 
 layoutdir=~/.screenlayout 
 
-DMENU='dmenu -i -nb #990000 -nf #999999 -sb #999999 -sf #000000'
-#choice=$(echo -e "notebook\nworkstation\nreboot\nsuspend\nhibernate" | $DMENU)
+#DMENU='dmenu -i -nb #990000 -nf #999999 -sb #999999 -sf #000000'
+DMENU='dmenu -i'
 
-choice=$((cd $layoutdir && ls -1|perl -pe 's/\.sh$//') | $DMENU)
+choice=$((echo "abort";cd $layoutdir && ls -1|perl -pe 's/\.sh$//') | $DMENU)
 
 script="$layoutdir/${choice}.sh"
 if [ -x "$script" ]; then
